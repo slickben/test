b8693d30962bdf6b3963bb5f3e9d22487d6b0b32
benson momodu <benson.momodu@minyx360.com>
2021-06-02 13:37:02 +0100

    installation and setup of nuxtjs and tailwind

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container flex items-center justify-center mx-auto h-screen">
+    <div>
+      <Logo />
+      <h1 class="text-center py-4 text-7xl uppercase font-bold">itams</h1>
+      <div class="">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="border-green-700 border p-4 py-2 rounded text-xl mx-4 hover:bg-green-700 hover:text-white"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="border-gray-700 border p-4 py-2 rounded text-xl mx-4 hover:bg-gray-700 hover:text-white"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>

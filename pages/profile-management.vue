8f2632bed892252b2655b7819202b41c4c90ad89
benson momodu <benson.momodu@minyx360.com>
2021-06-17 17:30:09 +0100

    created profile manager page

---
+++ pages/profile-management.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex flex-no-wrap bg-gray-200 h-screen">
+        <!-- Sidebar Start -->
+        <Sidebar />
+        <!-- Sidebar Ends -->
+        <div class="w-full">
+            <!-- Navigation starts -->
+            <Navigation />
+            <!-- Navigation ends -->
+            <div class="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
+                <!-- Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border -->
+                <nuxt-child />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import Sidebar from "~/components/SideBar.vue"
+import Navigation from "~/components/Navigation.vue"
+
+export default {
+    name: "ProfileManager",
+    components: {
+        Sidebar,
+        Navigation
+    },
+};
+</script>

866f7d83890510bca37765f61efbdf4e53898008
benson momodu <benson.momodu@minyx360.com>
2021-06-22 11:15:50 +0100

    update route to the new file and folder created

--- components/NavBox.vue
+++ components/NavBox.vue
@@ -1,14 +1,28 @@
 <template>
   <nuxt-link :to="link" class="text-center">
     <div>
-        <span :class="css" class="py-4 px-5 text-white rounded-md mx-auto w-24 h-24 flex items-center justify-center">
-            <div
-            class="shadow-2xl"
-            v-html="$feathericons[icon].toSvg({ width: 45, height: 45 })"
-            >
-            </div>
-        </span>
-        <p class="font-bold text-gray-100 pt-1 text-sm">{{ name }}</p>
+      <span
+        :class="css"
+        class="
+          py-4
+          px-5
+          text-white
+          rounded-md
+          mx-auto
+          w-24
+          h-24
+          flex
+          items-center
+          justify-center
+          hover:shadow-xl
+        "
+      >
+        <div
+          class="shadow-2xl"
+          v-html="$feathericons[icon].toSvg({ width: 45, height: 45 })"
+        ></div>
+      </span>
+      <p class="font-bold text-gray-100 pt-1 text-sm">{{ name }}</p>
     </div>
   </nuxt-link>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,52 @@
 <template>
-  <div class="container flex items-center justify-center mx-auto h-screen">
+  <div class="container flex items-start pt-20 justify-center mx-auto h-screen">
     <img
       class="w-screen h-full absolute inset-0 -z-10"
       src="~/assets/images/overlay-1.svg"
       alt=""
       srcset=""
     />
-    <div class="absolute inset-0 bg-gray-600 -z-10 opacity-80"></div>
-    <div class="grid grid-cols-5 gap-12 gap-x-16">
-      <NavBox css="bg-purple-900" link="/" name="Vehicle Operation" icon="hard-drive" />
-      <NavBox css="bg-yellow-900" link="/" name="Document Processing" icon="file-text" />
-      <NavBox css="bg-blue-900" link="/" name="Users Management" icon="users" />
+    <div class="absolute inset-0 bg-gray-900 -z-10 opacity-80"></div>
+    <div class="grid grid-cols-5 gap-12 gap-x-16 grid-rows-2">
+      <NavBox
+        css="bg-purple-900"
+        link="/"
+        name="Vehicle Operation"
+        icon="hard-drive"
+      />
+      <NavBox
+        css="bg-yellow-900"
+        link="/"
+        name="Document Processing"
+        icon="file-text"
+      />
+      <NavBox
+        css="bg-blue-900"
+        link="/users"
+        name="Users Manager"
+        icon="users"
+      />
       <NavBox css="bg-red-700" link="/" name="Profiling" icon="database" />
-      <NavBox css="bg-yellow-700" link="/" name="Vehicle" icon="home" />
-      <NavBox css="bg-gray-700" link="/" name="Business Intelligence" icon="activity" />
+      <NavBox
+        css="bg-yellow-700"
+        link="/owner-manager"
+        name="Owner Manager"
+        icon="home"
+      />
+      <NavBox
+        css="bg-gray-700"
+        link="/"
+        name="Business Intelligence"
+        icon="activity"
+      />
       <NavBox css="bg-green-700" link="/" name="Plate" icon="sliders" />
       <NavBox css="bg-yellow-700" link="/" name="Payment" icon="credit-card" />
-      <NavBox css="bg-yellow-700" link="/" name="Settings" icon="settings" />
+      <NavBox
+        css="bg-yellow-700"
+        link="/settings"
+        name="Settings"
+        icon="settings"
+      />
     </div>
   </div>
 </template>

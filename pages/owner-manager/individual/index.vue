73d289f660937be1e20204e7be8a608afe0b4642
benson momodu <benson.momodu@minyx360.com>
2021-06-22 11:12:03 +0100

    rename profile-management to owner-manager

---
+++ pages/owner-manager/individual/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full h-full rounded border-dashed border-2 border-gray-300">
+        Child page
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>

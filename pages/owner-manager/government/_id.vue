807dbed16fe0ba2d5d28c30eff129f6db576ab7a
benson momodu <benson.momodu@minyx360.com>
2021-07-08 18:15:12 +0100

    created owner-manager goverment page

---
+++ pages/owner-manager/government/_id.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-full h-full rounded border-dashed border-2 border-gray-300">
+        {{ $route.params.id }}
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Show',
+    asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
+        
+    },
+}
+</script>

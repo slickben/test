446cc89630cf949ab53c99f26ce29756fb1ba76e
benson momodu <benson.momodu@minyx360.com>
2021-07-08 18:09:57 +0100

    create logo component

---
+++ components/Logo.vue
@@ -0,0 +1,14 @@
+<template>
+    <nuxt-link class="" to="/">
+        <img v-if="size === 'big'" src="~/assets/images/Logo.svg" alt="" srcset="" />
+        <img v-if="size === 'small'" src="~/assets/images/LogoSmall.svg" alt="" srcset="" />
+    </nuxt-link>
+</template>
+
+<script>
+export default {
+    props: {
+        size: String
+    }
+}
+</script>

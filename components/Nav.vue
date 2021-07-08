aa9a380578c3adc00ea538abfb27f4766ac8ea42
benson momodu <benson.momodu@minyx360.com>
2021-07-08 18:10:20 +0100

    create nav component

---
+++ components/Nav.vue
@@ -0,0 +1,27 @@
+<template>
+    <li class="pl-5 hover:bg-primary-100 text-tertiary-500 my-2">
+        <nuxt-link :to="link" class="inline-flex items-center py-2 pb-3 mt-2 text-sm transition duration-500 ease-in-out transform w-full" href="#">
+            <slot></slot>
+            <span v-show="toggleNav" class="ml-4">{{ name }}</span>
+        </nuxt-link>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            String,
+            required: true
+        },
+        link: {
+            String,
+            required: true
+        },
+        toggleNav: {
+            String,
+            required: true
+        },
+    }
+}
+</script>

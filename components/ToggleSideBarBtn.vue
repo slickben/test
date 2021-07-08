dfe357f52924e10403e1bcdef2317ec798377f05
benson momodu <benson.momodu@minyx360.com>
2021-07-08 18:12:10 +0100

    create toggle side bar buttton component

---
+++ components/ToggleSideBarBtn.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="toggleNav" class="focus:outline-none">
+        <img src="~/assets/icons/menu.svg" alt="" srcset="">
+    </button>
+</template>
+
+<script>
+
+import { mapMutations } from 'vuex'
+
+export default {
+    methods: {
+        ...mapMutations({
+        toggleNav: 'toggleNav',
+        }),
+    }
+}
+</script>

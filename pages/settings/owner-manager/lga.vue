13ec95977fc057a7c0231e89eb243eb5511a15ed
benson momodu <benson.momodu@minyx360.com>
2021-07-14 10:33:43 +0100

    update owner settings sidebar nav

--- pages/settings/owner-manager/lga.vue
+++ pages/settings/owner-manager/lga.vue
@@ -1,8 +1,41 @@
 <template>
     <div class="flex h-full mr-24">
         <div class="flex-none w-72 bg-white h-full sidebar-shadow">
-            <SubSideBar> 
-
+            <SubSideBar title="Owner Manager"> 
+                <nav class="pt-3">
+                    <ul class=" text-tertiary-500">
+                        <li
+                        class="w-full border-b hover:border-transparent"
+                        >
+                        <nuxt-link class="py-3 px-6 border-l-8 border-transparent hover:text-primary-500 hover:bg-primary-100 w-full hover:border-primary-500 block" to="/settings/owner-manager/state">State</nuxt-link>
+                        </li>
+                        <li
+                        class="w-full border-b hover:border-transparent "
+                        >
+                        <nuxt-link class="py-3 px-6 border-l-8 border-transparent hover:text-primary-500 hover:bg-primary-100 w-full hover:border-primary-500 block" to="/settings/owner-manager/lga">Local Government Area</nuxt-link>
+                        </li>
+                        <li
+                        class="w-full border-b hover:border-transparent "
+                        >
+                        <nuxt-link class="py-3 px-6 border-l-8 border-transparent hover:text-primary-500 hover:bg-primary-100 w-full hover:border-primary-500 block" to="/settings/owner-manager/prefix">Prefix</nuxt-link>
+                        </li>
+                        <li
+                        class="w-full border-b hover:border-transparent "
+                        >
+                        <nuxt-link class="py-3 px-6 border-l-8 border-transparent hover:text-primary-500 hover:bg-primary-100 w-full hover:border-primary-500 block" to="/settings/owner-manager/marital-status">Marital Status</nuxt-link>
+                        </li>
+                        <li
+                        class="w-full border-b hover:border-transparent "
+                        >
+                        <nuxt-link class="py-3 px-6 border-l-8 border-transparent hover:text-primary-500 hover:bg-primary-100 w-full hover:border-primary-500 block" to="/settings/owner-manager/agency-category">Agency Ministry</nuxt-link>
+                        </li>
+                        <li
+                        class="w-full border-b hover:border-transparent "
+                        >
+                        <nuxt-link class="py-3 px-6 border-l-8 border-transparent hover:text-primary-500 hover:bg-primary-100 w-full hover:border-primary-500 block" to="/settings/owner-manager/means-of-identity">Means of Identification</nuxt-link>
+                        </li>
+                    </ul>
+                </nav>
             </SubSideBar>
         </div>
         <div class="flex-grow">

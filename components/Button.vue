addf585b172490a6185645a30a0757453caa23ff
benson momodu <benson.momodu@minyx360.com>
2021-08-19 17:00:43 +0100

    polish on change of ownership process & renewal process

--- components/Button.vue
+++ components/Button.vue
@@ -1,9 +1,9 @@
 <template>
-    <button @click="onClick" v-if="type === 'solid'" class="px-6 min-w-28 min-h-10 text-white text-xs flex items-center justify-center bg-primary-400 focus:outline-none border-0 mr-4 hover:bg-primary-600">{{ title }}</button>
+    <button @click.prevent="onClick" v-if="type === 'solid'" class="px-6 min-w-28 min-h-10 text-white text-xs flex items-center justify-center bg-primary-400 focus:outline-none border-0 mr-4 hover:bg-primary-600">{{ title }}</button>
 
-    <button @click="onClick" v-else-if="type === 'secondary'" class="px-6 min-w-28 min-h-10 text-primary-500 text-xs flex items-center justify-center  focus:outline-none border border-primary-300 hover:bg-primary-200 hover:text-primary-600"> {{ title }} </button>
+    <button @click.prevent="onClick" v-else-if="type === 'secondary'" class="px-6 min-w-28 min-h-10 text-primary-500 text-xs flex items-center justify-center  focus:outline-none border border-primary-300 hover:bg-primary-200 hover:text-primary-600"> {{ title }} </button>
 
-    <button @click="onClick" v-else class="px-6 min-w-28 min-h-10 text-primary-500 text-xs flex items-center justify-center  focus:outline-none border-0 hover:bg-primary-100"> {{ title }} </button>
+    <button @click.prevent="onClick" v-else class="px-6 min-w-28 min-h-10 text-primary-500 text-xs flex items-center justify-center  focus:outline-none border-0 hover:bg-primary-100"> {{ title }} </button>
 
 </template>
 
--- components/form/InputWithButtonRight.vue
+++ components/form/InputWithButtonRight.vue
@@ -5,7 +5,7 @@
         <div class="flex items-center w-full">
             <input v-bind:value="value"
             v-on:input="$emit('input', $event.target.value)" class="w-full focus:outline-none py-3 px-6 border border-r-0 rounded-tl rounded-bl text-xs text-tertiary-300 font-normal placeholder-tertiary-300" :type="type"  :id="id" :placeholder="place_holder">
-            <button @click="onClick" class="text-xs focus:outline-none px-12 bg-primary-400 py-3 text-white border border-primary-400 rounded-tr rounded-br"> {{ BtnTitle }} </button>
+            <button @click.prevent="onClick" class="text-xs focus:outline-none px-12 bg-primary-400 py-3 text-white border border-primary-400 rounded-tr rounded-br"> {{ BtnTitle }} </button>
         </div>
     </div>
 </template>
